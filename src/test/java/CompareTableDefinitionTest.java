2678f7d0f9b50b3cfd0e908c2d9d08b8a4a37f87
Durga <107354364+Durga-dp@users.noreply.github.com>
2023-04-12 13:10:31 -0400

    Update CompareTableDefinitionTest.java

--- src/test/java/CompareTableDefinitionTest.java
+++ src/test/java/CompareTableDefinitionTest.java
@@ -6,8 +6,8 @@ public class CompareTableDefinitionTest {
 
     @Test
     public void testCompareTablesInDatabases(){
-        String databaseConnectionUrl1="jdbc:postgresql://localhost:5432/postgres?user=mani&password=abc123";
-        String databaseConnectionUrl2="jdbc:postgresql://localhost:5432/postgres?user=mani&password=abc123";
+        String databaseConnectionUrl1="jdbc:postgresql://localhost:5432/postgres?user=durga&password=abc123";
+        String databaseConnectionUrl2="jdbc:postgresql://localhost:5432/postgres?user=durga&password=abc123";
         String table1="em";
         String table2="em";
         compareTableDefinition=new CompareTableDefinition();
